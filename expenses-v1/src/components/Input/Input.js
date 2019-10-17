b0fc4a9d741dea1e90fb4615ce18ae140b2e17dd
Aðalsteinn Jóhannsson <aj1.h18@nemandi.ntv.is>
2019-10-17 20:44:59 +0000

    Verkefni breytt í functional úr class

--- expenses-v1/src/components/Input/Input.js
+++ expenses-v1/src/components/Input/Input.js
@@ -1,36 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Input.css';
+import styled from 'styled-components';
 
-class Input extends React.Component {
-  render() {
-    return (
-      <div className="Input-container">
-        <label className="Input-label" htmlFor={this.props.name}>
-          {this.props.name}
-        </label>
-        <input
-          name={this.props.name}
-          className="Input"
-          type="text"
-          value={this.props.value}
-          onChange={e => this.props.handleChange(e)}
-        />
-      </div>
-    );
-  }
-}
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  width: 5em;
+`;
+
+const Input = ({ name, value, handleChange }) => (
+  <Container>
+    <Label htmlFor={name}>{name}</Label>
+    <input
+      name={name}
+      className="Input"
+      type="text"
+      value={value}
+      onChange={e => handleChange(e)}
+    />
+  </Container>
+);
 
 Input.propTypes = {
   name: PropTypes.string,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  handleChange: PropTypes.func,
+  handleChange: PropTypes.func
 };
 
 Input.defaultProps = {
   name: '',
   value: '',
-  handleChange: () => {},
+  handleChange: () => {}
 };
 
 export default Input;
